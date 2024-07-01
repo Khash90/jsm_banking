a80550fae4decffb9ccec4ebaeb37c87a63b5522
Khash <90476560+Khash90@users.noreply.github.com>
2024-07-01 18:18:22 +0330

    Make sidebar for desktop and prepare it for mobileview

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
@@ -11,7 +12,7 @@ const Sidebar = ({ user }:  SiderbarProps ) => {
   return (
    <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+            <Link href='/' className='flex mb-12 cursor-pointer items-center gap-2'>
             <Image src='/icons/logo.svg' width={34} height={34} alt='logo' className='size-[24px] max-xl:size-14' />
             <h1 className='sidebar-logo'>Horizon</h1>
             </Link>
@@ -22,11 +23,30 @@ const Sidebar = ({ user }:  SiderbarProps ) => {
                 <Link href={item.route} key={item.label} className={cn('sidebar-link', {
                     'bg-bank-gradient': isActive
                 })}>
-                    {item.label}
+                    <div className='relative size-6'>
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          fill
+                          className={cn({
+                            'brightness-[3]':
+                            isActive
+                          })}
+                        />
+                    </div>
+                    <p className={cn('sidebar-label', {
+                      '!text-white': isActive
+                    })}>
+                        {item.label}
+                    </p>
                 </Link>
               )  
             })}
+
+            USER
         </nav>
+
+        FOOTER
    </section>
   )
 }
